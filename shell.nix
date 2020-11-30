819d947f18937d1541bb0ca2d4afcfc6f4df90fb
ttak0422 <bgdaewalkman@gmail.com>
2020-12-01 04:30:49 +0900

    :snowflake: setup nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
 let 
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs {
+    overlays = [ 
+      (import ./nix/dotnet-sdk.nix) 
+    ]; 
+  };
 in pkgs.mkShell {
   buildInputs = with pkgs; [
+    dotnet-sdk
+    nodejs
     # keep this line if you use bash
     bashInteractive
   ];
