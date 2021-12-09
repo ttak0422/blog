53288ed9229c523297ac6964944bca9d869e2169
ttak0422 <ttak0422@gmail.com>
2021-12-10 00:42:50 +0900

    :+1: apply patch

--- flake.nix
+++ flake.nix
@@ -15,15 +15,16 @@
       inherit (builtins) fetchTarball;
       inherit (flake-utils.lib) eachDefaultSystem eachSystem allSystems;
       # WIP......
-      stack = fetchTarball {
-        url =
-          "https://github.com/CaiJimmy/hugo-theme-stack/archive/refs/heads/master.tar.gz";
-        sha256 = "12r53vmnfjv8140qnw7a3qhi0xy2lff3acfadliksv5kkjhm9kqi";
-      };
+      # stack = fetchTarball {
+      #   url =
+      #     "https://github.com/CaiJimmy/hugo-theme-stack/archive/refs/heads/master.tar.gz";
+      #   sha256 = "12r53vmnfjv8140qnw7a3qhi0xy2lff3acfadliksv5kkjhm9kqi";
+      # };
     in eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) mkShell runCommand callPackage;
+        stack = callPackage ./nix/hugo-theme-stack { };
       in {
         devShell = mkShell {
           buildInputs = with pkgs; [ hugo nixfmt pre-commit ];
